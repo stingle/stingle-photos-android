48b5f7f75270ba11b4be140f43aa1f7b1b70572b
Alex Amiryan <alex@amiryan.org>
2012-07-30 15:43:47 +0400

    Built import activity, but it sometimes messes thumbnail with real path

--- src/com/fenritz/safecam/ImportPhotosActivity.java
+++ src/com/fenritz/safecam/ImportPhotosActivity.java
@@ -2,97 +2,148 @@ package com.fenritz.safecam;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ImportPhotosActivity extends Activity {
+	private int count;
+	private Bitmap[] thumbnails;
+	private boolean[] thumbnailsselection;
+	private String[] arrPath;
+	private ImageAdapter imageAdapter;
 
-	private GridView photosGrid;
-	private Cursor cursor;
-	private int columnIndex;
-
+	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.import_photos);
 
-		   // Set up an array of the Thumbnail Image ID column we want
-	    String[] projection = {MediaStore.Images.Thumbnails._ID};
-	    // Create the cursor pointing to the SDCard
-	    cursor = managedQuery( MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,
-	            projection, // Which columns to return
-	            null,       // Return all rows
-	            null,
-	            MediaStore.Images.Thumbnails.IMAGE_ID);
-	    // Get the column index of the Thumbnails Image ID
-	    columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Thumbnails._ID);
+		final String[] columns = { MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID };
+		final String orderBy = MediaStore.Images.Media.DATE_TAKEN;
+		Cursor imagecursor = managedQuery(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns, null, null, orderBy);
+		int image_column_index = imagecursor.getColumnIndex(MediaStore.Images.Media._ID);
+		this.count = imagecursor.getCount();
+		this.thumbnails = new Bitmap[this.count];
+		this.arrPath = new String[this.count];
+		this.thumbnailsselection = new boolean[this.count];
+		for (int i = 0; i < this.count; i++) {
+			imagecursor.moveToPosition(i);
+			int id = imagecursor.getInt(image_column_index);
+			int dataColumnIndex = imagecursor.getColumnIndex(MediaStore.Images.Media.DATA);
+			thumbnails[i] =
+					MediaStore.Images.Thumbnails.getThumbnail(getApplicationContext().getContentResolver(), id,
+							MediaStore.Images.Thumbnails.MICRO_KIND, null);
+			arrPath[i] = imagecursor.getString(dataColumnIndex);
+		}
+		GridView imagegrid = (GridView) findViewById(R.id.PhoneImageGrid);
+		imageAdapter = new ImageAdapter();
+		imagegrid.setAdapter(imageAdapter);
+		imagecursor.close();
 
-	    GridView sdcardImages = (GridView) findViewById(R.id.photosGrid);
-	    sdcardImages.setAdapter(new ImageAdapter(this));
+		final Button selectBtn = (Button) findViewById(R.id.selectBtn);
+		selectBtn.setOnClickListener(new OnClickListener() {
 
-	    // Set up a click listener
-	    sdcardImages.setOnItemClickListener(new OnItemClickListener() {
-	        public void onItemClick(AdapterView parent, View v, int position, long id) {
-	            // Get the data location of the image
-	            String[] projection = {MediaStore.Images.Media.DATA};
-	            cursor = managedQuery( MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-	                    projection, // Which columns to return
-	                    null,       // Return all rows
-	                    null,
-	                    null);
-	            columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-	            cursor.moveToPosition(position);
-	            // Get image filename
-	            String imagePath = cursor.getString(columnIndex);
-	            // Use this path to do further processing, i.e. full screen display
-	        }
-	    });
+			public void onClick(View v) {
+				final int len = thumbnailsselection.length;
+				int cnt = 0;
+				String selectImages = "";
+				for (int i = 0; i < len; i++) {
+					if (thumbnailsselection[i]) {
+						cnt++;
+						selectImages = selectImages + arrPath[i] + "|";
+					}
+				}
+				if (cnt == 0) {
+					Toast.makeText(getApplicationContext(), "Please select at least one image", Toast.LENGTH_LONG).show();
+				}
+				else {
+					Toast.makeText(getApplicationContext(), "You've selected Total " + cnt + " image(s).", Toast.LENGTH_LONG).show();
+					Log.d("SelectedImages", selectImages);
+				}
+			}
+		});
 	}
 
-	/**
-	 * Adapter for our image files.
-	 */
-	private class ImageAdapter extends BaseAdapter {
+	public class ImageAdapter extends BaseAdapter {
+		private final LayoutInflater mInflater;
 
-	    private final Context context;
+		public ImageAdapter() {
+			mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		}
 
-	    public ImageAdapter(Context localContext) {
-	        context = localContext;
-	    }
+		public int getCount() {
+			return count;
+		}
 
-	    public int getCount() {
-	        return cursor.getCount();
-	    }
-	    public Object getItem(int position) {
-	        return position;
-	    }
-	    public long getItemId(int position) {
-	        return position;
-	    }
-	    public View getView(int position, View convertView, ViewGroup parent) {
-	        ImageView picturesView;
-            picturesView = new ImageView(context);
-            // Move cursor to current position
-            cursor.moveToPosition(position);
-            // Get the current value for the requested column
-            int imageID = cursor.getInt(columnIndex);
-            // Set the content of the image based on the provided URI
-            picturesView.setImageURI(Uri.withAppendedPath(
-                    MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, "" + imageID));
-            picturesView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-            picturesView.setPadding(8, 8, 8, 8);
-            //picturesView.setLayoutParams(new GridView.LayoutParams(300, 300));
-	        return picturesView;
-	    }
+		public Object getItem(int position) {
+			return position;
+		}
+
+		public long getItemId(int position) {
+			return position;
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder holder;
+			holder = new ViewHolder();
+			View view = mInflater.inflate(R.layout.gallery_item, null);
+			holder.imageview = (ImageView) view.findViewById(R.id.thumbImage);
+			holder.checkbox = (CheckBox) view.findViewById(R.id.itemCheckBox);
+
+			view.setTag(holder);
+			
+			holder.checkbox.setId(position);
+			holder.imageview.setId(position);
+			holder.checkbox.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					CheckBox cb = (CheckBox) v;
+					int id = cb.getId();
+					if (thumbnailsselection[id]) {
+						cb.setChecked(false);
+						thumbnailsselection[id] = false;
+					}
+					else {
+						cb.setChecked(true);
+						thumbnailsselection[id] = true;
+					}
+				}
+			});
+			
+			final int thisPosition = position;
+			holder.imageview.setOnClickListener(new OnClickListener() {
+
+				public void onClick(View v) {
+					Intent intent = new Intent();
+					intent.setAction(Intent.ACTION_VIEW);
+					intent.setDataAndType(Uri.parse("file://" + arrPath[thisPosition]), "image/*");
+					startActivity(intent);
+				}
+			});
+			holder.imageview.setImageBitmap(thumbnails[position]);
+			holder.checkbox.setChecked(thumbnailsselection[position]);
+			holder.id = position;
+			return view;
+		}
 	}
 
+	class ViewHolder {
+		ImageView imageview;
+		CheckBox checkbox;
+		int id;
+	}
 }
