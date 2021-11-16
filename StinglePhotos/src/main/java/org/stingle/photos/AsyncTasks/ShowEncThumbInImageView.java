b35b844584a8b1626504c77e7fcaace142ba64c6
Smbat Sargsyan <smbatsargsyan1992@gmail.com>
2021-11-16 19:16:10 +0400

    - Fixed thumbnail circle image

--- StinglePhotos/src/main/java/org/stingle/photos/AsyncTasks/ShowEncThumbInImageView.java
+++ StinglePhotos/src/main/java/org/stingle/photos/AsyncTasks/ShowEncThumbInImageView.java
@@ -2,6 +2,11 @@ package org.stingle.photos.AsyncTasks;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.os.AsyncTask;
 import android.widget.ImageView;
 
@@ -103,10 +108,30 @@ public class ShowEncThumbInImageView extends AsyncTask<Void, Void, Bitmap> {
 		super.onPostExecute(bitmap);
 
 		if(bitmap != null){
-			imageView.setImageBitmap(bitmap);
+			imageView.setImageBitmap(getCroppedBitmap(bitmap));
 			if(onFinish != null){
 				onFinish.onFinish();
 			}
 		}
+	}
+
+	public Bitmap getCroppedBitmap(Bitmap bitmap) {
+		Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+				bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+		Canvas canvas = new Canvas(output);
+
+		final int color = 0xff424242;
+		final Paint paint = new Paint();
+		final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+		paint.setAntiAlias(true);
+		canvas.drawARGB(0, 0, 0, 0);
+		paint.setColor(color);
+
+		canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
+				bitmap.getWidth() / 2, paint);
+		paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+		canvas.drawBitmap(bitmap, rect, rect, paint);
+		return output;
 	}
 }
