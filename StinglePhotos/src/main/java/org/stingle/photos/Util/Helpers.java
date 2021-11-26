3040cc1efcd48ca6a8001eba824301125f6eedec
Alex Amiryan <alex@amiryan.org>
2021-11-26 04:51:31 +0400

    Android 12 upgrade and long click on date to select all from date

--- StinglePhotos/src/main/java/org/stingle/photos/Util/Helpers.java
+++ StinglePhotos/src/main/java/org/stingle/photos/Util/Helpers.java
@@ -55,6 +55,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
